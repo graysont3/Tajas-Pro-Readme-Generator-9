affd86e651f86f24a98081f30719827b6a97b6c3
Star Child <tajagrayson@Tajas-MacBook-Air.local>
2023-04-02 12:04:40 -0400

    added questions, writeToFile function, and init function

--- index.js
+++ index.js
@@ -17,14 +17,42 @@ things to ask:
 - How do I contact you for questions? (email, and github)
 */
 const questions = [
-    
+    {
+        type: "input",
+        message: "What is the title of your app?",
+        name: "projectTitle"
+    },
+    {
+        message: "What is a short description of your project?",
+        name: "shortDescription"
+    }
+
+
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log("created file")
+    })
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+        console.log(answers)
+        const template= `# Title of my README
+
+## Description
+
+My dog's name is ${answers.dogName} and he is a great dog.`
+
+        writeToFile("sampleReadme.md", template)
+    })
+
+}
 
 // Function call to initialize app
 init();
